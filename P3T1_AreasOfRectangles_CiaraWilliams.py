862fd0f242da5ae71f4a8f18908f97e3ad3ded4a
CiaraW-tech <55807575+CiaraW-tech@users.noreply.github.com>
2019-09-25 14:18:13 -0400

    Add files via upload

---
+++ P3T1_AreasOfRectangles_CiaraWilliams.py
@@ -0,0 +1,26 @@
+# This program is going to get the dimensions of a rectangle
+# Ciara Williams
+# 250919
+
+length1 = int(input('Enter the length of rectangle 1: '))
+width1 = int(input('Eneter the width of recatngle 1: '))
+
+
+# Get the dimesions  of rectangle 2.
+length2 = int(input('Enter the length of rectangle 2: '))
+width2 = int(input('Enter the width of rectangle 2: '))
+
+
+# Calclate the areas of the rectangles.
+area1 = length1 * width1
+area2 = length2 * width2
+
+
+# Determine which has the greater area.
+if area1 > area2:
+    print('Rectangle 1 has the greater area.')
+elif:
+    if area2 > area1:
+        print ('Rectangle 2 has the greater area.')
+    else:
+        print('Both have the same area.')
